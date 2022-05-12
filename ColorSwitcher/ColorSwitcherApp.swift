141a0cf4c1e55f50b0a733a60b433d27b9c48042
Sergey <kopgamerk@gmail.com>
2022-05-12 17:25:19 +0300

    Initial Commit

---
+++ ColorSwitcher/ColorSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSwitcherApp.swift
+//  ColorSwitcher
+//
+//  Created by Сергей Веретенников on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
